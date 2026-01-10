86a1513e94b2730d71e5282159d62c1838eac3e8
Faddah Wolf <my_biz@me.com>
2026-01-09 19:19:23 -0800

    Created About Page.

---
+++ shopping-cart/src/pages/About.tsx
@@ -0,0 +1,8 @@
+export function About() {
+    return (
+        <div>
+            <h1>Welcome to the About Page</h1>
+            <p>This is the About page of the Shopping Cart Application.</p>
+        </div>
+    );
+}
