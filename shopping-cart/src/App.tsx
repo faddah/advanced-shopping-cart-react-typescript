98dfa293e663cb8b5ffede29699e34801d85d9da
Faddah Wolf <my_biz@me.com>
2026-01-09 21:16:48 -0800

    Deleted commented-out content.

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -14,14 +14,6 @@ function App() {
       <Route path="/about" element={<About/>} /> 
     </Routes>
   </Container>
-
-  // (
-  //   <div className="App">
-  //     <img src={reactLogo} className="react-logo" alt="React.JS Logo Graphic" />
-  //     <h1>Shopping Cart Application</h1>
-  //     <img src="https://dijf55il5e0d1.cloudfront.net/images/na/1/7/3/17368_1000.jpg" className="shopping-cart-image" alt="Shopping Cart Picture" />
-  //   </div>
-  // )
 }
 
 export default App
