188524cdb4e196391fa700d26122dea9a01d88e4
Faddah Wolf <my_biz@me.com>
2026-01-09 19:23:46 -0800

    Deleted Shopping Cart Application content & moved to pages/Home.tsx; now returns the <Container> and all <Routes> within.

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -7,13 +7,21 @@ import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 
 function App() {
-  return (
-    <div className="App">
-      <img src={reactLogo} className="react-logo" alt="React.JS Logo Graphic" />
-      <h1>Shopping Cart Application</h1>
-      <img src="https://dijf55il5e0d1.cloudfront.net/images/na/1/7/3/17368_1000.jpg" className="shopping-cart-image" alt="Shopping Cart Picture" />
-    </div>
-  )
+  return <Container>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/store" element={<Store/>} /> 
+      <Route path="/about" element={<About/>} /> 
+    </Routes>
+  </Container>
+
+  // (
+  //   <div className="App">
+  //     <img src={reactLogo} className="react-logo" alt="React.JS Logo Graphic" />
+  //     <h1>Shopping Cart Application</h1>
+  //     <img src="https://dijf55il5e0d1.cloudfront.net/images/na/1/7/3/17368_1000.jpg" className="shopping-cart-image" alt="Shopping Cart Picture" />
+  //   </div>
+  // )
 }
 
 export default App
