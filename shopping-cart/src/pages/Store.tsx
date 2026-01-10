aedd21f046955b3b10fa5afd3416f0846fcfbb6e
Faddah Wolf <my_biz@me.com>
2026-01-09 19:19:33 -0800

    Created Store Page

---
+++ shopping-cart/src/pages/Store.tsx
@@ -0,0 +1,8 @@
+export function Store() {
+    return (
+        <div>
+            <h1>Welcome to the Store Page</h1>
+            <p>This is the online Store page of the Shopping Cart Application.</p>
+        </div>
+    );
+}
