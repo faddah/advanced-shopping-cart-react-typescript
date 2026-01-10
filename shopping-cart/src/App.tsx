d7afe76b3a588d95857bde896da6e39de616a49c
Faddah Wolf <my_biz@me.com>
2026-01-09 16:33:15 -0800

    Added import of react.svg as `reactLogo`, then used that in the <img> tag to make sure SVG showd up on page.

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
   return (
     <div className="App">
+      <img src={reactLogo} className="react-logo" alt="React.JS Logo Graphic" />
       <h1>Shopping Cart Application</h1>
       <img src="https://dijf55il5e0d1.cloudfront.net/images/na/1/7/3/17368_1000.jpg" className="shopping-cart-image" alt="Shopping Cart Picture" />
     </div>
