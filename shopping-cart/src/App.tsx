355c5541f6ed2d32f807a488e16b126614399a45
Faddah Wolf <my_biz@me.com>
2026-01-11 17:56:58 -0800

    Imported NavBar and added it to the App.

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -2,18 +2,22 @@ import './App.css'
 // import reactLogo from './assets/react.svg'
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { NavBar } from "./components/NavBar";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 
 function App() {
-  return <Container>
+  return <>
+  <NavBar />
+  <Container>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/store" element={<Store/>} /> 
       <Route path="/about" element={<About/>} /> 
     </Routes>
   </Container>
+  </>
 }
 
 export default App
