2be370b922584e70325b3599a03324f3fe90d77b
Faddah Wolf <my_biz@me.com>
2026-01-11 17:37:05 -0800

    Created NavBar() for navigation at top of page.

---
+++ shopping-cart/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { Home } from "../pages/Home";
+import { Store } from "../pages/Store";
+import { About } from "../pages/About";
+
+export function NavBar() {
+    return (
+        <h1>NavBar</h1>
+        // <nav>
+        //     <ul>
+        //         <li><a href="/">{ <Home /> }</a></li>
+        //         <li><a href="/store">{ <Store /> }</a></li>
+        //         <li><a href="/about">{ <About /> }</a></li>
+        //     </ul>
+        // </nav>
+    );
+}
