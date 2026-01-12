e821c8ca1dbb974c82790525b441bbb1095cf207
Faddah Wolf <my_biz@me.com>
2026-01-12 00:06:49 -0800

    Created Shopping Cart w/ <Offcanvas> & <Stack> from react-bootstrap to have a right-side slider to show Shopping Cart and items within.

---
+++ shopping-cart/src/components/ShoppingCart.tsx
@@ -0,0 +1,35 @@
+import { Offcanvas, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "../components/CartItem";
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type ShoppingCartProps = {
+    isOpen: boolean;
+}
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+    const { closeCart, cartItems } = useShoppingCart();
+    const total = cartItems.reduce((total, cartItem) => {
+        const item = storeItems.find(i => i.id === cartItem.id);
+        return total + (item?.price || 0) * cartItem.quantity;
+    }, 0);
+    return (
+        <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Your Shopping Cart</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Stack gap={3}>
+                    {cartItems.map(item => (
+                        <CartItem key={item.id} {...item} />
+                    ))}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total{" "}
+                        {formatCurrency(total)}
+                    </div>
+                </Stack>
+            </Offcanvas.Body>
+        </Offcanvas>
+    );
+}
