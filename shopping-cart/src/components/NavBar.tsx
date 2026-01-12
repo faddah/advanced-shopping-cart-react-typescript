c5aed6c9eb169d04567373b6a7662f2014ee9130
Faddah Wolf <my_biz@me.com>
2026-01-12 00:04:33 -0800

    Refactoring of Button / SVG to View the Shopping Cart.

--- shopping-cart/src/components/NavBar.tsx
+++ shopping-cart/src/components/NavBar.tsx
@@ -15,11 +15,11 @@ export function NavBar() {
                 {cartQuantity > 0 && (
                     <Button
                         style={{ width: "3rem", height: "3rem", position: "relative" }}
-                    variant="outline-primary"
-                    className="rounded-circle"
-                    onClick={openCart}
-                    aria-label="View cart"
-                >
+                        variant="outline-primary"
+                        className="rounded-circle"
+                        onClick={openCart}
+                        aria-label="View cart"
+                    >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 576 512"
