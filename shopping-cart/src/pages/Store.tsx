fc8ec2679ded3833bc7084303f3f12d8098eb789
Faddah Wolf <my_biz@me.com>
2026-01-11 20:16:33 -0800

    Added necessary imports and fleshed out the Store page with react-bootstrap Row, Column, and a storeItems.map(...).

--- shopping-cart/src/pages/Store.tsx
+++ shopping-cart/src/pages/Store.tsx
@@ -1,8 +1,17 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+
 export function Store() {
     return (
         <div>
             <h1>Welcome to the Store Page</h1>
             <p>This is the online Store page of the Shopping Cart Application.</p>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map(item => (
+                    <Col key={item.id}><StoreItem {...item} /></Col>
+                ))}
+            </Row>
         </div>
     );
 }
