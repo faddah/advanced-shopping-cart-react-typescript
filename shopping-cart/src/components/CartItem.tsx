4599ed54c69fa42f0cda674053ab41bb78d19eb7
Faddah Wolf <my_biz@me.com>
2026-01-13 17:50:58 -0800

    Imported new findById & idDefined from /utilities/typeGuards.

--- shopping-cart/src/components/CartItem.tsx
+++ shopping-cart/src/components/CartItem.tsx
@@ -3,6 +3,7 @@ import storeItems from "../data/storeItems";
 import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { findById, isDefined } from "../utilities/typeGuards";
 
 type CartItemProps = {
     id: number;
