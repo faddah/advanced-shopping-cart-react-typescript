ba5b733ba97df3b9987e2f59ec3d43464f787f1f
Faddah Wolf <my_biz@me.com>
2026-01-13 23:22:07 -0800

    Imported useMemo hook from React.

--- shopping-cart/src/context/ShoppingCartContext.tsx
+++ shopping-cart/src/context/ShoppingCartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useMemo } from "react";
 import { ShoppingCart } from "../components/ShoppingCart";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import {
